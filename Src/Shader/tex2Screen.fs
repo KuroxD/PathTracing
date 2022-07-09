326bfa486d2c98a25a324f96ad5aecbfdcb2a078
KuroxD <623921119@qq.com>
2022-07-09 22:54:19 +0800

    ACES tone mapping

--- Src/Shader/tex2Screen.fs
+++ Src/Shader/tex2Screen.fs
@@ -4,16 +4,20 @@ out vec4 FragColor;
 
 uniform sampler2D curFrameTex;
 
-vec3 toneMapping(in vec3 c, float limit) {
-    float luminance = 0.3*c.x + 0.6*c.y + 0.1*c.z;
-    return c * 1.0 / (1.0 + luminance / limit);
+vec3 ACESFilm(vec3 x)
+{
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+    return clamp((x*(a*x+b))/(x*(c*x+d)+e),0,1);
 }
-
 
 void main()
 {
     vec3 color=texture2D(curFrameTex,gl_FragCoord.xy/textureSize(curFrameTex,0)).rgb;
-    color=toneMapping(color,1.5);
+    color=ACESFilm(color);
     color=pow(color,vec3(1.0/2.2));
     FragColor=vec4(color,1);
 }
